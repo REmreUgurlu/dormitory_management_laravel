1900859ad41f8a86151abe06a6700268baf06025
Emre_Ugurlu <koroglu-314@windowslive.com>
2023-12-03 21:55:56 +0300

    Everything set up for livewire

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    $students = \App\Models\Student::all();
+ 
+    return view('welcome', ['students' => $students]);
+});
